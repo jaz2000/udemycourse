ac5b2b35f30923f8038cdc76b73793ad76f5805b
Jazeel Anwar <Jazeel Anwar@KCT-L-527>
2023-08-21 15:33:02 +0530

    Add project files.

---
+++ udemy/Person.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    private int salary {get; set; } 
+}
---
+++ udemy/program.cs
@@ -0,0 +1,6 @@
+﻿
+
+Person person = new();
+person.LastName = Console.ReadLine();
+person.FirstName = Console.ReadLine();
+Console.WriteLine(person.FirstName);
